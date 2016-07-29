54177c91ce7b07d77cda7e447a6e1b0b607a33ee
Student <student@D-173-250-189-156.dhcp4.washington.edu>
2016-07-29 15:23:23 -0700

    first commit

---
+++ Assets/EmptyClass.cs
@@ -0,0 +1,23 @@
+﻿
+using UnityEngine;
+using System.Collections;
+
+public class Enemy_AI : MonoBehaviour {
+
+	Transform tr_Player;
+	float f_RotSpeed=100000000000.0f,f_MoveSpeed = 100000000000.0f;
+
+	// Use this for initialization
+	void Start () {
+
+		tr_Player = GameObject.FindGameObjectWithTag ("ThirdPersonCharacter").transform; }
+
+	// Update is called once per frame
+	void Update () {
+		/* Look at Player*/
+		transform.rotation = Quaternion.Slerp (transform.rotation , Quaternion.LookRotation (tr_Player.position - transform.position) , f_RotSpeed * Time.deltaTime);
+
+		/* Move at Player*/
+		transform.position += transform.forward * f_MoveSpeed * Time.deltaTime;
+	}
+}
